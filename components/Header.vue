04aa73e095ec1d1794fa199e6f2c48b86d138287
nguyen van thai <nguyenvanthailtv@gmail.com>
2024-03-31 17:35:00 +0700

    .

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,55 @@
 <script setup lang="ts">
-import img from "@/assets/images/_636903097234341637.png";
+import img from "@/assets/images/logo_header.jpg";
 </script>
 <template>
-  <header class="container mx-auto">
-    <section class="flex items-center justify-between">
+  <header class="b">
+  <div class="container mx-auto ">
+    <section class=" flex items-center justify-between gap-5">
       <figure>
-        <img :src="img" class="w-[100px]" alt="">
+        <img :src="img" class="h-[54px] object-cover" alt="">
       </figure>
+
+      <ul class="flex items-center gap-2 flex-1">
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Trang Chủ
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Chương Trình Đào Tạo
+          </nuxt-link>
+          </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Đăng Kí
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Sinh Viên
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Đánh Giá
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Thông Tin
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="#" class="font-semibold hover:text-primary transition duration-200 ease-linear">
+            Thông Tin Tài Chính
+          </nuxt-link>
+        </li>
+      </ul>
+
+      <nuxt-link to="login">Đăng Nhập</nuxt-link>
     </section>
+  </div>
   </header>
 </template>
 
