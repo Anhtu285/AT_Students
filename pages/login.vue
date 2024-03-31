54120a979112badf1118e5d90ecf365a80e36c11
AnhTu <trananhtu280502@gamil.com>
2024-03-31 10:49:32 +0700

    n

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="bg-gray-200 w-full h-screen text-primary font-sans">
     <div>tessttt</div>
+    <img :src="img" alt="" />
   </div>
 </template>
 
 <script setup lang="ts">
+import img from "@/assets/images/_636903097234341637.png";
 definePageMeta({
   layout: "login",
 });
